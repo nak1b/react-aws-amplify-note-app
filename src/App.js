ca10f6d10709259388b54b6ae700e4502131dae2
Nakib Momin <nbmomin14@gmail.com>
2019-04-23 19:07:20 -0400

    updating and deleting note

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { withAuthenticator } from 'aws-amplify-react'
 import { API, graphqlOperation } from 'aws-amplify'
-import { createNote } from './graphql/mutations'
+import { createNote, deleteNote, updateNote } from './graphql/mutations'
 import { listNotes } from './graphql/queries'
 
 class App extends React.Component {
   state = {
     note: '',
-    notes: []
+    notes: [],
+    id: ''
   }
 
   async componentDidMount() {
@@ -21,11 +22,24 @@ class App extends React.Component {
     this.setState({note: e.target.value})
   }
 
+  _isNoteExists = () => {
+    const { id, notes } = this.state 
+
+    if(!id) { return false }
+
+    return notes.findIndex(note => note.id === id) > -1 
+  }
+
   _handleSubmit = async (e) => {
     const { note } = this.state
     e.preventDefault()
 
+    if(this._isNoteExists()) {
+      return this._updateNote()
+    }
+
     const input = { note }
+
     const res = await API.graphql(graphqlOperation(createNote, { 
       input 
     }))
@@ -37,13 +51,53 @@ class App extends React.Component {
     })
   }
 
+  _updateNote = async () => {
+    const { note, id } = this.state
+    const input = { note, id }
+
+    const res = await API.graphql(graphqlOperation(updateNote, {
+      input
+    }))
+
+    const updatedNote = res.data.updateNote
+    const updatedNotes = this.state.notes.map(note => {
+      return note.id === id ? updatedNote : note
+    })
+
+    this.setState({
+      notes: updatedNotes,
+      note: '',
+      id: ''
+    })
+  }
+
+  _handleDelete = async (id) => {
+    const input = { id }
+
+    const res = await API.graphql(graphqlOperation(deleteNote, { input }))
+    const deletedNoteId = res.data.deleteNote.id
+
+    const newNotes = this.state.notes.filter(note => note.id !== deletedNoteId)
+    this.setState({notes: newNotes})
+  }
+
+  _handleNoteSelected = ({note, id}) => {
+    this.setState({ note, id })
+  }
+
   _renderNotes = () => {
     const { notes } = this.state 
     
     return notes.map(note => (
       <div key={note.id} className='flex items-center'>
-        <li className='list pa3 f4'>{note.note}</li>
-        <button className='bg-transparent bn f4'>
+        <li 
+          className='list pa3 f4'
+          onClick={() => this._handleNoteSelected(note)}>
+          {note.note}
+        </li>
+        <button 
+          className='bg-transparent bn f4'
+          onClick={() => this._handleDelete(note.id)}>
           <span>&times;</span>
         </button>
       </div>
